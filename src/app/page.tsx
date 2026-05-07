375298574d25251c883dac16df2521569c89de81
Merano19 <onaremmikmik12@gmail.com>
2026-05-07 14:54:34 +0800

    fix: add missing useEffect import for StarField component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
